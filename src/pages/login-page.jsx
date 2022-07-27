794e144824e1d9dfd420860a2c494b006d5aff14
Yulia Lushinskaya <crimson-alloya@yandex.ru>
2022-07-27 16:22:06 +0400

    react-hook-form

--- src/pages/login-page.jsx
+++ src/pages/login-page.jsx
@@ -15,9 +15,6 @@ export function LoginPage() {
     setValue({ ...form, [e.target.name]: e.target.value });
   };
 
-  const handleLogin = () =>   {  
-    dispatch(login(form));
-  }
 
   if (isAuth) {
     return (
@@ -25,9 +22,14 @@ export function LoginPage() {
     )
   }
 
+  const onSubmit = (e, data) => {  
+    e.preventDefaul();
+    console.log(data);
+    dispatch(login(form));
+  }
   return (
     <div className={`${s.container} ${s.centered}`}>
-      <div className={s.content}>
+      <form className={s.content} onSubmit={(e) => onSubmit(e, form)}>
         <div className="text text_type_main-medium pb-6">Вход</div>
         <div className={`${s.input} pb-6`}>
           <Input onChange={onChange} value={form.email} name={'email'} placeholder={'E-mail'} />
@@ -36,12 +38,12 @@ export function LoginPage() {
           <PasswordInput onChange={onChange} value={form.password} name={'password'} />
         </div>
 
-        <Button type="primary" size="medium" onClick={handleLogin} disabled={loginRequest}>
+        <Button type="primary" size="medium" disabled={loginRequest}>
           Войти
         </Button>
         <p className="text text_type_main-default text_color_inactive pt-20 pb-4">Вы - новый пользователь? <Link to="/register" className={s.link}>Зарегистрироваться</Link></p>
         <p className="text text_type_main-default text_color_inactive">Забыли пароль? <Link to="/forgot-password" className={s.link}>Восстановить пароль</Link></p>
-      </div>
+      </form>
     </div>
   )
 }
--- src/pages/registration-page.jsx
+++ src/pages/registration-page.jsx
@@ -3,11 +3,15 @@ import { useState } from "react";
 import { createUser } from "../utils/api";
 import s from './page.module.css';
 import { Link, Redirect, useLocation } from 'react-router-dom';
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { useForm } from "react-hook-form";
+import { registration } from "../services/actions/auth";
 
 export function RegistrationPage() {
+  const dispatch = useDispatch();
   const { state } = useLocation();
   const { isAuth } = useSelector(store => store.auth);
+  const { register, handleSubmit, formState: { errors } } = useForm();
 
   const [form, setValue] = useState({ name: '', email: '', password: '' });
 
@@ -15,10 +19,10 @@ export function RegistrationPage() {
     setValue({ ...form, [e.target.name]: e.target.value });
   };
 
-  const register = () => {
-    createUser(form)
-      .then()
-      .catch(err => console.log(err))
+  const onSubmit = (e, data) => {
+    e.preventDefault();
+    console.log(data);
+    dispatch(registration(form))
   }
 
   if (isAuth) {
@@ -29,23 +33,32 @@ export function RegistrationPage() {
 
   return (
     <div className={`${s.container} ${s.centered}`}>
-      <div className={s.content}>
+      <form className={s.content} onSubmit={(e) => onSubmit(e, form)}>
         <div className="text text_type_main-medium pb-6">Регистрация</div>
         <div className={`${s.input} pb-6`}>
-          <Input onChange={onChange} value={form.name} name={'name'} placeholder={'Имя'}/>
+          <Input onChange={onChange} value={form.name} name={'name'} placeholder={'Имя'} type="text"/>
         </div>
         <div className={`${s.input} pb-6`}>
-          <Input onChange={onChange} value={form.email} name={'email'} placeholder={'E-mail'} />
+          <Input onChange={onChange} value={form.email} name={'email'} placeholder={'E-mail'} type="email" />
         </div>
         <div className={`${s.input} pb-6`}>
           <PasswordInput onChange={onChange} value={form.password} name={'password'} />
         </div>
+        {/* <div className={`${s.input} pb-6`}>
+          <Input value={form.name} name={'name'} placeholder={'Имя'} {...register('name')}/>
+        </div>
+        <div className={`${s.input} pb-6`}>
+          <Input value={form.email} name={'email'} placeholder={'E-mail'} type="email" {...register('email')} />
+        </div>
+        <div className={`${s.input} pb-6`}>
+          <PasswordInput type="password" value={form.password} name={'password'} {...register('password')} />
+        </div> */}
 
-        <Button type="primary" size="medium" onClick={register}>
+        <Button type="primary" size="medium" >
           Зарегистрироваться
         </Button>
         <p className="text text_type_main-default text_color_inactive pt-20 pb-4">Уже зарегистрированы? <Link to={{ pathname: `/login`, state }} className={s.link}>Войти</Link></p>
-      </div>
+      </form>
     </div>
   )
 }
